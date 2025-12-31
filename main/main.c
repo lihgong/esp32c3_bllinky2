0471ada3087984c73c8d2c13b04e3447b4f4b6d1
lihgong <lihgong@gmail.com>
2026-01-01 00:59:30 +0800

    Initial check-in of the LED blinky project that runs against ESP-IDF

---
+++ main/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+static const char *TAG = "LED_BLK";
+static uint8_t s_led_state = 0;
+
+#define LED_BLINK_PERIOD (250)
+#define BLINK_GPIO (13)
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+static void blink_led(void)
+{
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+void app_main(void)
+{
+    configure_led();
+    while (1) {
+        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+        blink_led();
+        s_led_state = !s_led_state; /* Toggle the LED state */
+        vTaskDelay(LED_BLINK_PERIOD / portTICK_PERIOD_MS);
+    }
+}
+
